339de99edcabc9ce5e3e251957a3d9b5c600d511
Jacek Schefler <zija1504@gmail.com>
2020-07-28 17:12:40 +0200

    Added Command Model to API project

---
+++ src/howTo/Models/Commands.cs
@@ -0,0 +1,10 @@
+namespace howTo.Models
+{
+    public class Commands
+    {
+        public int Id { get; set; }
+        public string HowTo { get; set; }
+        public string Platform { get; set; }
+        public string CommandLine { get; set; }
+    }
+}
